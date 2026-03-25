9a04ca06c8316f38369db179ceb678982b9bd6a2
Mahesh <allisonlucifer82@gmail.com>
2026-03-25 11:10:35 +0530

    Added railway database credentials in DBConfiguration Class

--- src/main/java/com/smartcity/util/DBConnection.java
+++ src/main/java/com/smartcity/util/DBConnection.java
@@ -2,24 +2,22 @@ package com.smartcity.util;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.SQLException;
 
 public class DBConnection {
-	private static String url="jdbc:postgresql://localhost:5433/smartcity?sslmode=disable";
-	private static String username="postgres";
-	private static String password="123";
-	
 
-	
-	public static Connection getConnection(){
-		Connection conn=null;
-		try {
-				Class.forName("org.postgresql.Driver");
-				 conn=DriverManager.getConnection(url,username,password);
-		}
-		catch(SQLException | ClassNotFoundException e) {
-			e.printStackTrace();
-		}
-		return conn;
-	}
+    private static String url = "jdbc:postgresql://roundhouse.proxy.rlwy.net:30147/railway?sslmode=require";
+    private static String username = "postgres";
+    private static String password = "tkXtwSayDHQzqFfYZZfxxNKIIpneXdfG";
+
+    public static Connection getConnection() {
+        try {
+            Class.forName("org.postgresql.Driver");
+            Connection conn = DriverManager.getConnection(url, username, password);
+            System.out.println("✅ Connected to Railway DB");
+            return conn;
+        } catch (Exception e) {
+            e.printStackTrace(); // VERY IMPORTANT
+            throw new RuntimeException("DB connection failed");
+        }
+    }
 }
